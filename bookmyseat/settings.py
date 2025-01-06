96cbeeae2f761dbf6d2d2078dff0058484a2fd84
yogeshmanikanta <yogeshmanikanta.log8@gmail.com>
2025-01-06 19:08:38 +0530

    A

--- bookmyseat/settings.py
+++ bookmyseat/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = [
     '127.0.0.1',                 # For local development
     'localhost',                 # For local development
 ]
-
+ 
 
 
 # Application definition
@@ -135,7 +135,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
