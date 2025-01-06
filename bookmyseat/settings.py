c78c6c58f72ff16715569182c8d11d23f48fddea
yogeshmanikanta <yogeshmanikanta.log8@gmail.com>
2025-01-06 18:55:37 +0530

    Allowed hosts

--- bookmyseat/settings.py
+++ bookmyseat/settings.py
@@ -29,7 +29,12 @@ SECRET_KEY = 'django-insecure-c8aetlj(=vp90n@#yoc^&d(_6ivp(d!bv-4-f!r$lawptjzrwu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://bms-zeta-navy.vercel.app/']
+ALLOWED_HOSTS = [
+    'bms-zeta-navy.vercel.app',  # Your deployment domain
+    '127.0.0.1',                 # For local development
+    'localhost',                 # For local development
+]
+
 
 
 # Application definition
