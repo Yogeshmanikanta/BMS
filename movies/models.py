69e2b58ebba127bb5fd76367b98316505028c050
yogeshmanikanta <yogeshmanikanta.log8@gmail.com>
2025-01-20 17:46:11 +0530

    trailer model

--- movies/models.py
+++ movies/models.py
@@ -2,12 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+
 class Movie(models.Model):
-    name= models.CharField(max_length=255)
-    image= models.ImageField(upload_to="movies/")
-    rating = models.DecimalField(max_digits=3,decimal_places=1)
-    cast= models.TextField()
-    description= models.TextField(blank=True,null=True) # optional
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to="movies/")
+    rating = models.DecimalField(max_digits=3, decimal_places=1)
+    cast = models.TextField()
+    description = models.TextField(blank=True, null=True)  # optional
+    trailer = models.URLField(blank=True, null=True)  # optional field for trailer
+
 
     def __str__(self):
         return self.name
